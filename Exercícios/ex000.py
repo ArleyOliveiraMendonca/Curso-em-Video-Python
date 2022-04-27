a36f72018b43c27914c2c3ff674be2906c149f4f
ArleyOliveiraMendonca <arley@arley.com.br>
2022-04-27 18:27:28 -0300

    Atualização de Exercicios

--- Exercícios/ex000.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input('Digite o primeiro número '))
-num2 = int(input('Digie o segundo número '))
-
-soma = (num1) + (num2)
-# print ('A soma entre ', num1 ,' e ', num2 ,' vale ',soma)
-
-print('A soma entre {} e {} vale {}'.format(num1, num2, soma))
--- Exercícios/ex001.py
+++
@@ -1,2 +0,0 @@
-nome: input('Digite seu Nome: ')
-print('É um prazer te conhecer, ', nome)
