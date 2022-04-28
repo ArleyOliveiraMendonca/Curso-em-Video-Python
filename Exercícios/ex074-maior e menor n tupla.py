bb8b05fcc50a2a14c35fcc9a10d932615d7f74da
ArleyOliveiraMendonca <arley@arley.com.br>
2022-04-28 15:12:56 -0300

    Alguns ainda em andamento

--- Exercícios/ex074-maior e menor n tupla.py
+++ Exercícios/ex074-maior e menor n tupla.py
@@ -3,3 +3,11 @@ Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla
 
 Depois disso, mostre a listagem de numeros gerados e também indique o menor e o aior valor que estão na tupla.
 '''
+
+from random import randint
+n = (randint(1, 60), randint(1, 60), randint(1, 60), randint(1, 60), randint(1, 60)),
+print(f'Os valores sorteados foram: ', end='')
+for n in n:
+    print(f'{n} ', end='')
+print(f'\nO maior valor sorteado foi {max(n)}')
+print(f'O menor valor sorteado foi {min(n)}')
--- Exercícios/ex075-quatro-valores.py
+++ Exercícios/ex075-quatro-valores.py
@@ -1,7 +1,20 @@
 '''
-Desenvolva um programa que leia queto valores plo teclado e guarde-os em uma tupla. No final, mostre:
+Desenvolva um programa que leia quatro valores plo teclado e guarde-os em uma tupla. No final, mostre:
 
 A) Quantas vezes aparece o valor 9
 B) Em que posição foi digitado o primeiro valor 3
 C) Quais foram os números pares.
 '''
+
+n1 = (int(input('Digite um número: ')), int(input('Digite mais um número: ')),int(input('Digite outro número: ')), int(input('Digite o último número: ')))
+
+print(f'Você digitou os valores {n1}')
+print(f'Quantas vezes o valor 9 pareceu: {n1.count(9)}')
+if 3 in n1:
+    print(f'o valor 3 apareceu na {n1.index(3)+1}ª posição')
+else:
+    print('O valor 3 não foi digitado em nenhma posição')
+print('Os valores pares digitados foram ', end='')
+for n in n1:
+    if n % 2 == 0:
+        print(n , end=' ')
