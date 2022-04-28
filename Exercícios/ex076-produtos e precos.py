3b81e802d210eb4f566e338a3f73081f7f187053
ArleyOliveiraMendonca <arley@arley.com.br>
2022-04-28 17:51:30 -0300

    Alguns ainda em andamento

--- Exercícios/ex076-produtos e precos.py
+++ Exercícios/ex076-produtos e precos.py
@@ -3,3 +3,22 @@ Crie um programa que tenha uma tupla única com nomes de produtos e seus respect
 
 No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 '''
+
+listagem = ('Lápis', 1.75,
+            'Borracha', 2,
+            'Caderno', 15.99,
+            'Estojo', 25,
+            'Transferidor', 4.20,
+            'Compasso', 9.99,
+            'Mochila', 120.32,
+            'Canetas', 22.30,
+            'Livros', 34.90)
+print('-' * 40)
+print(f'{"Listagem de Preços":^40}')
+print('-' * 40)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
+print('-' * 40)
--- Exercícios/ex077-palavras-vogais.py
+++ Exercícios/ex077-palavras-vogais.py
@@ -1,4 +1,12 @@
 '''
-Crie um ptograma que tenha uma tupla com várias palavras (não use acentos).
+Crie um programa que tenha uma tupla com várias palavras (não use acentos).
 Depois disso, você deve mostrar para cada palavra, quais sao as suas vogais.
 '''
+
+palavras = ('aprender', 'programar', 'linguagem', 'pyhon', 'curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
+
+for p in palavras:
+    print(f'\nNa palavra {p} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou': #lower faz as letras serem todas minusculas
+            print(letra, end=' ')
