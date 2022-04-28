a1362ea5a203ae22051eeef25f424ad7a97c2b28
ArleyOliveiraMendonca <arley@arley.com.br>
2022-04-28 01:57:13 -0300

    Alguns ainda em andamento

--- Exercícios/ex072-por-extenso.py
+++ Exercícios/ex072-por-extenso.py
@@ -5,7 +5,10 @@ Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por
 '''
 
 numero = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezessei', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
-n = int(input('Digite um número entre [0 e 20] '))
-
+while True:
+    n = int(input('Digite um número entre [0 e 20] '))
+    if 0 <= n <=20:
+        break
+    print('Tente novamente. ', end='')
 print(f'Você digitou o número {numero[n]}')
 
