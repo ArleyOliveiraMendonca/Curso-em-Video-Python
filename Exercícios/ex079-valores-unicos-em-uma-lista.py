05a2d34f8067bd4b7c7c2a0d46aefef4c88c73ce
ArleyOliveiraMendonca <arley@arley.com.br>
2022-04-30 23:33:37 -0300

    Alguns ainda em andamento

--- Exercícios/ex079-valores-unicos-em-uma-lista.py
+++ Exercícios/ex079-valores-unicos-em-uma-lista.py
@@ -2,3 +2,19 @@
 Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado.
 No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
 '''
+
+numeros = list()
+while True:
+    n = str(input('Digite um valor: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado! não vou adicionar...')
+
+    r = str(input('Quer continuar? [S / N]'))
+    if r in 'Nn':
+        break
+print('-=' * 30)
+numeros.sort()
+print(f'Voce digitou os valores {numeros}')
