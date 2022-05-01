db6b7abbfa3ff70c52a4ac231de853ff88b20fb4
ArleyOliveiraMendonca <arley@arley.com.br>
2022-05-01 00:13:24 -0300

    Alguns ainda em andamento

--- Exercícios/ex081-extraindo-dads-de-uma-lista.py
+++ Exercícios/ex081-extraindo-dads-de-uma-lista.py
@@ -7,3 +7,17 @@ A) Qauntos número foram digitados.
 B) A lista de valores, ordenada de forma decrescente
 C) Se o valor 5 foi digitado e esta ou não na lista
 '''
+valores = []
+while True:
+    valores.append(int(input('Digite um valor: ')))
+    resp = str(input('Quer continuar? [S/N]'))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'Voce digitou {len(valores)} elementos.')
+valores.sort(reverse=True)
+print(f'Os valores em ordem decrescente são {valores}')
+if 5 in valores:
+    print('O valor 5 faz parte da lista!')
+else:
+    print('O valor 5 não foi encontrado na lista')
