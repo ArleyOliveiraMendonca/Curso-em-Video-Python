59e17568aacc48020712a08ab5b9c5c8fd6dcbeb
ArleyOliveiraMendonca <arley@arley.com.br>
2022-05-02 01:37:28 -0300

    Alguns ainda em andamento

---
+++ Exercícios/ex083-validando-mtematica.py
@@ -0,0 +1,20 @@
+'''
+Exercício Python 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
+Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+'''
+
+expr = str(input('Digite a expressão: '))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('A sua expressão está válida!')
+else:
+    print('A sua expressão esta invalida!')
