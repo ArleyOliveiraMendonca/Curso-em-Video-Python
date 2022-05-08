ebf93275e699a8f89eef097a213232bd9186d3b7
ArleyOliveiraMendonca <arley@arley.com.br>
2022-05-08 19:01:10 -0300

    Alguns ainda em andamento

--- python-teste/aula019.py
+++ python-teste/aula019.py
@@ -1,2 +1,42 @@
+'''
 pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade':22}
+pessoas['nome'] = 'Arley'
+pessoas['peso'] = 98.5
 print(pessoas)
+print(pessoas['nome'])
+print(pessoas['idade'])
+print(f'O {pessoas["nome"]} tem {pessoas["idade"] }anos.')
+print(pessoas.keys())
+print(pessoas.values())
+print(pessoas.items())
+for k in pessoas.keys():
+    print(k)
+for k in pessoas.values():
+    print(k)
+for k, v in pessoas.items():
+    print(f'{k} = {v}')
+
+brasil = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+
+print(estado1)
+print(estado2)
+print(brasil)
+print(brasil[0])
+print(brasil[0]['uf'])
+print(brasil[1]['uf'])
+print(brasil[0]['sigla'])
+print(brasil[1]['sigla'])
+'''
+estado = dict()
+brasil = list()
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do Estado: '))
+    brasil.append(estado.copy())
+for e in brasil:
+    for k, v in e.items():
+        print(f'O campo {k} tem valor {v}')
