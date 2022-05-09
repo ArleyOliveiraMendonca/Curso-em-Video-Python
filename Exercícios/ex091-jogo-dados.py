a471753f1f96f391d990441da6b5dd5c62f15ecd
ArleyOliveiraMendonca <arley@arley.com.br>
2022-05-08 22:29:38 -0300

    Alguns ainda em andamento

---
+++ Exercícios/ex091-jogo-dados.py
@@ -0,0 +1,23 @@
+'''
+Exercício Python 091: Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios.
+Guarde esses resultados em um dicionário em Python. No final, coloque esse dicionário em ordem, sabendo
+que o vencedor tirou o maior número no dado.
+'''
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogo = {'jogador1': randint(1, 6),
+        'jogador2': randint(1, 6),
+        'jogador3': randint(1, 6),
+        'jogador4': randint(1, 6)}
+ranking = list()
+print('Valores sorteado: ')
+for k, v in jogo.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(0.3)
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print('-=' * 30)
+print('  -= RANKING DOS JOGADORES =-')
+for i, v in enumerate(ranking):
+    print(f'  {i+1}º lugar: {v[0]} com {v[1]}')
+    sleep(0.3)
