6e09ea28ad8ac232937f2ebb2aef5b995a6e85a3
ArleyOliveiraMendonca <arley@arley.com.br>
2022-05-08 22:02:49 -0300

    Alguns ainda em andamento

---
+++ Exercícios/ex090-dicionario.py
@@ -0,0 +1,16 @@
+'''
+Exercício Python 090: Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
+No final, mostre o conteúdo da estrutura na tela.
+'''
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['media'] >= 7:
+    aluno['situacao'] = 'Aprovado'
+elif 5 <= aluno['media']:
+    aluno['situacao'] = 'Recuperação'
+else:
+    aluno['situacao'] = 'Reprovado'
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f'  - {k} é igual a {v}')
